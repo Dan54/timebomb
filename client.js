239e19a15aae854240e1182f38054ba5f7d4655a
Dan54 <dannyjwheeler@hotmail.co.uk>
2025-06-26 01:28:08 +0100

    Card picking

--- client.js
+++ client.js
@@ -1,12 +1,80 @@
 import { sendToServer, clientHandlers } from "./comms.js";
 
+const cardBack = '&#x1F0A0;';
+
+let myId = -1;
+let players = new Map();
+let picker = -1;
+
 clientHandlers['chat'] = function(data) {
     console.log(data);
 };
 
 clientHandlers['connect'] = function(data) {
     console.log(`Connected to server, we are ${data.id}`);
+    myId = data.id;
 };
+
+function createOtherPlayer(id) {
+    let ops = document.getElementById('otherPlayers');
+    ops.insertAdjacentHTML("beforeend", `<div class="player">
+            <div class="name"></div>
+            <div class="displayHand"></div>
+        </div>`);
+    players.set(id, ops.lastElementChild);
+}
+
+function setDisplayHand(playerId, hand) {
+    let handDiv = players.get(playerId).getElementsByClassName('displayHand')[0];
+    handDiv.innerHTML = ''; // clear the hand first
+    for (let index = 0; index < hand.length; index++) {
+        const card = hand[index];
+        const span = document.createElement('span');
+        span.innerHTML = card;
+        handDiv.appendChild(span);
+        if (playerId !== myId && card === cardBack) {
+            span.addEventListener('click', () => {
+                if (picker === myId) {
+                    sendToServer('pick-card', {pickee: playerId, cardIndex: index});
+                }
+            });
+        }
+    }
+}
+
+clientHandlers['start-round'] = function(cardsPerPlayer) {
+    let displayHand = new Array(cardsPerPlayer).fill(cardBack);
+    players.keys().forEach((playerId) => {
+        setDisplayHand(playerId, displayHand);
+    });
+};
+
+clientHandlers['start-game'] = function(data) {
+    document.getElementById('otherPlayers').innerHTML = '';
+    data.players.forEach(pid => {
+        if (pid !== myId || true) {
+            createOtherPlayer(pid);
+        }
+    });
+    picker = data.firstPlayer;
+    players.get(picker).classList.add('inPower');
+};
+
+clientHandlers['set-role'] = function(role) {
+
+};
+
+clientHandlers['set-cards'] = function(cards) {
+
+};
+
+clientHandlers['card-picked'] = function(data) {
+    players.get(picker).classList.remove('inPower');
+    picker = data.pickee;
+    setDisplayHand(data.pickee, data.hand);
+    players.get(picker).classList.add('inPower');
+};
+
 
 export function sendMessage(msg) {
     sendToServer('chat', msg);
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import { createServer, serverHandlers, sendToClient, broadcast, connectToLocal } from "./comms.js";
 
+let canJoin = true;
+let players = new Map();
+
 serverHandlers['connect'] = function(id, data) {
-    console.log(`Connected to client ${id}`);
+    if (canJoin) {
+        players.set(id, {});
+    }
 };
 
 serverHandlers['chat'] = function(id, data) {
@@ -14,3 +19,107 @@ export function startServer() {
     });
     connectToLocal();
 }
+
+let blacksLeft = 0;
+let jokersLeft = 0;
+let primerLeft = 0;
+let numPlayers = 0;
+let curPicker = 0;
+let pickedThisRound = 0;
+const cardBack = '&#x1F0A0;';
+let firstPlayer = -1;
+
+let goodIn = 0;
+let badIn = 0;
+let redAceIn = 0;
+let blackAceIn = 0;
+let cardsPerPlayer = 4;
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+export function startGame() {
+    canJoin = false;
+    numPlayers = players.size;
+    blacksLeft = numPlayers;
+    jokersLeft = 2;
+    primerLeft = 1;
+    if (goodIn + badIn + redAceIn + blackAceIn < numPlayers) {
+        if (numPlayers === 4 || numPlayers === 5) {
+            goodIn = 4;
+            badIn = 2;
+            redAceIn = 1;
+            blackAceIn = 0;
+        }
+        badIn = (numPlayers + 2) / 3;
+        goodIn = badIn * 2;
+        redAceIn = 1;
+        blackAceIn = 1;
+    }
+    let roleList = new Array(goodIn).fill('good').concat(new Array(badIn).fill('bad')).concat(new Array(redAceIn).fill('red-ace')).concat(new Array(blackAceIn).fill('black-ace'));
+    curPicker = players.keys().next().value;
+    firstPlayer = curPicker;
+    broadcast('start-game', {firstPlayer: curPicker, players: Array.from(players.keys())});
+    shuffleArray(roleList);
+    players.forEach((playerData, id) => { // (value, key) for some reason
+        let role = roleList.pop();
+        playerData.role = role;
+        sendToClient(id, 'set-role', role);
+    });
+    startRound();
+}
+
+function startRound() {
+    let cardList = new Array(numPlayers * cardsPerPlayer).fill('R');
+    let i = 0;
+    pickedThisRound = 0;
+    for (let c = 0; c < blacksLeft; c++) {
+        cardList[i++] = 'B';
+    }
+    for (let c = 0; c < jokersLeft; c++) {
+        cardList[i++] = 'J';
+    }
+    for (let c = 0; c < primerLeft; c++) {
+        cardList[i++] = '&clubs;';
+    }
+    cardList[i] = '&spades;';
+    shuffleArray(cardList);
+    i = 0;
+    broadcast('start-round', cardsPerPlayer);
+    players.forEach((playerData, id) => {
+        let cards = cardList.slice(i, i + 4);
+        i += 4;
+        playerData.cards = cards;
+        playerData.displayHand = new Array(cardsPerPlayer).fill(cardBack);
+        sendToClient(id, 'set-cards', cards);
+    });
+}
+
+function pickCard(id, data) {
+    if (id !== curPicker) {
+        return;
+    }
+    let pickee = data.pickee;
+    if (pickee == id) {
+        return;
+    }
+    let cardIndex = data.cardIndex;
+    let cards = players.get(pickee).cards;
+    let displayHand = players.get(pickee).displayHand;
+    if (cardIndex < 0 || cardIndex >= displayHand.length || displayHand[cardIndex] !== cardBack) {
+        return;
+    }
+    shuffleArray(cards);
+    let card = cards.pop();
+    displayHand[cardIndex] = card;
+    curPicker = pickee;
+    broadcast('card-picked', {picker: id, pickee: pickee, hand: displayHand});
+    sendToClient(pickee, 'set-cards', cards);
+}
+serverHandlers['pick-card'] = pickCard;
